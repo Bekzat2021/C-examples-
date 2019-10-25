c7d79ab04b3fffd3670e97342b76f207d0680a87
Bekzat2021 <bekzat.work@gmail.com>
2019-10-25 23:09:22 +0500

    Task No 5 No ended

--- Hello.cpp
+++ Hello.cpp
@@ -1,18 +1,32 @@
 // Hello.cpp : This file contains the 'main' function. Program execution begins and ends there.
 #include "pch.h"
 #include <iostream>
+
 using namespace std;
 
-template <typename T>
-class EverythingButInt {
+template <typename T1, typename T2>
+class TemplateArray {
+private:
+	T1* firstArray;
+	T2* secondArray;
+
 public:
-	EverythingButInt() {
-		static_assert(sizeof(T) != sizeof(int), "No int please!");
+	TemplateArray(T1* firstInput, T2* secondInput):firstArray(firstInput), secondArray(secondInput){}
+
+	void ShowFirstArr() {
+		for (T1 f: firstArray)
+		{
+			cout << f << endl;
+		}
 	}
 };
 
 int main() {
-	EverythingButInt<double> Test;
+	
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	int arr2[10] = { 1,2,3,4,5,6,7,8,9,10 };
+
+	TemplateArray<int, int> arr(arr, arr2);
 	
 	return 0;
 }
