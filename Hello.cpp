0a06a5afbb0611f086d7d0fb593a8be53f7abd95
Bekzat2021 <bekzat.work@gmail.com>
2019-10-29 22:51:16 +0500

    Listing 16.8 Using operator s introdiced by C++14

--- Hello.cpp
+++ Hello.cpp
@@ -1,36 +1,17 @@
 // Hello.cpp : This file contains the 'main' function. Program execution begins and ends there.
 
 #include "pch.h"
-#include <iostream>
 #include <string>
+#include <algorithm>
+#include <iostream>
 
 int main() {
 
-	std::string sampleStr("Good day String! Today is beautiful!");
-	std::cout << "Sample string is: " << sampleStr << std::endl << std::endl;
+	std::string str1("Traditional string \0 initialization");
+	std::cout << "Str1: " << str1 << " Length: " << str1.length() << std::endl;
 
-	size_t charPos = sampleStr.find("day", 0);
-
-	if (charPos!=std::string::npos)
-	{
-		std::cout << "First instance \"day\" at pos. " << charPos << std::endl;
-	}
-	else 
-	{
-		std::cout << "Substring not found!" << std::endl;
-	}
-
-	std::cout << "Locating all instances of substring \"day\"" << std::endl;
-	size_t subStrPos = sampleStr.find("day", 0);
-
-	while (subStrPos!=std::string::npos)
-	{
-		std::cout << "\"day\" found at position " << subStrPos << std::endl;
-
-		size_t searchOffset = subStrPos + 1;
-
-		subStrPos = sampleStr.find("day", searchOffset);
-	}
+	std::string str2("C++14 \0 initialization using literals");
+	std::cout << "Str2: " << str2 << " Length: " << str2.length() << std::endl;
 
 	return 0;
 }
