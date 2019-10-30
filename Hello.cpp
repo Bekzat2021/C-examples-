56c19c73231775694a1db19b4d63205fc65802e6
Bekzat2021 <bekzat.work@gmail.com>
2019-10-30 23:04:26 +0500

    Exercises from STL string class 2 not end

--- Hello.cpp
+++ Hello.cpp
@@ -1,17 +1,63 @@
 // Hello.cpp : This file contains the 'main' function. Program execution begins and ends there.
 
 #include "pch.h"
+#include <iostream>
 #include <string>
 #include <algorithm>
-#include <iostream>
+#include <cctype>
+
+using namespace std;
 
 int main() {
 
-	std::string str1("Traditional string \0 initialization");
-	std::cout << "Str1: " << str1 << " Length: " << str1.length() << std::endl;
+	cout << "What do you want to do \n ";
+	cout << "1: check word to polindrome \n";
+	cout << "2: vowels in sentence \n";
+	int userSelection = 0;
+	cin >> userSelection;
 
-	std::string str2("C++14 \0 initialization using literals");
-	std::cout << "Str2: " << str2 << " Length: " << str2.length() << std::endl;
+	switch (userSelection)
+	{
+	case 1:{
+		unsigned char userRepeatPolindrome = 'y';
+		while (userRepeatPolindrome =='y')
+		{
+			cout << "Enter a word: ";
+			string inputWord;
+			cin >> inputWord;
+
+			string reversedWord = inputWord;
+
+			reverse(reversedWord.begin(), reversedWord.end());
+
+			if (reversedWord == inputWord)
+			{
+				cout << "Congragulations word is polindrome!" << endl;
+			}
+			else
+			{
+				cout << "Input word is not polindrome " << endl;
+			}
+			
+			cout << "Do you want to repeat opeartaion (y/n) ?" << endl;
+			cout << "> ";
+			cin >> userRepeatPolindrome;	
+		}	
+		break;
+	}
+	case 2: {
+		cout << "Enter a sentence: ";
+		string wowSentence;
+		cin >> wowSentence;
+
+
+	}
+		
+	default:
+		break;
+	}
+
+	
 
 	return 0;
 }
