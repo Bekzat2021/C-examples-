674c2612b9f0590291fca68457efa7c56ad91806
Bekzat2021 <bekzat.work@gmail.com>
2019-10-31 22:00:18 +0500

    LISTING 17.3 Using the vector::insert Function to Insert Elements at a Set Position

--- Hello.cpp
+++ Hello.cpp
@@ -2,169 +2,40 @@
 
 #include "pch.h"
 #include <iostream>
-#include <string>
-#include <algorithm>
+#include <vector>
 
 using namespace std;
 
+void DisplayVector(const vector<int>& inVec) {
+	
+	for (auto element=inVec.begin(); element!=inVec.end(); element++)
+	{
+		cout << *element << ' ';
+	}
+
+	cout << endl;
+}
+
 int main() {
 
-	cout << "What do you want to do \n";
-	cout << "1: Check word to polindrome \n";
-	cout << "2: Vowels in sentence \n";
-	cout << "3: Convert all characters to uppercase \n";
-	cout << "4: Show string \n";
-	cout << "5: Show all occurences \n";
-	cout << "6: Show all occurences not my implementation \n";
+	vector<int> integers(4, 90);
 
-	int userSelection = 0;
-	cin >> userSelection;
+	cout << "The initial contents of the vector: ";
+	DisplayVector(integers);
 
-	switch (userSelection)
-	{
-	case 1:{
-		unsigned char userRepeatPolindrome = 'y';
-		while (userRepeatPolindrome =='y')
-		{
-			cout << "Enter a word: ";
-			string inputWord;
-			cin >> inputWord;
+	integers.insert(integers.begin(), 25);
 
-			string reversedWord = inputWord;
+	integers.insert(integers.end(), 2, 45);
 
-			reverse(reversedWord.begin(), reversedWord.end());
+	cout << "Vector after inserting elements at the beginning and end: ";
+	DisplayVector(integers);
 
-			if (reversedWord == inputWord)
-			{
-				cout << "Congragulations word is polindrome!" << endl;
-			}
-			else
-			{
-				cout << "Input word is not polindrome " << endl;
-			}
-			
-			cout << "Do you want to repeat opeartaion (y/n) ?" << endl;
-			cout << "> ";
-			cin >> userRepeatPolindrome;	
-		}	
-		break;
-	}
-	case 2: {
-		unsigned char userRepeatVow = 'y';
-		while (userRepeatVow == 'y')
-		{
-			cout << "Enter a sentence: ";
-			string wowSentence;
-			cin >> wowSentence;
+	vector<int> another(2, 30);
 
-			char vowels[5] = { 'e', 'y', 'o', 'a', 'u' };
-			int vow = 0;
+	integers.insert(integers.begin() + 1, another.begin(), another.end());
 
-			for (int j = 0; j < wowSentence.length(); j++)
-			{
-				for (int i = 0; i < 5; i++)
-				{
-					if (wowSentence[j] == vowels[i])
-					{
-						vow++;
-					}
-				}
-			}
-		
-			cout << "In sentence you was entered " << vow << " vowels" << endl;
-			cout << "Do you want to repeat opeartaion (y/n) ?" << endl;
-			cout << "> ";
-			cin >> userRepeatVow;
-		}
-	}
-	case 3: {
-		unsigned char userRepeatToupper = 'y';
-		while (userRepeatToupper == 'y')
-		{
-			cout << "Enter a word: ";
-			string str;
-			cin >> str;
-
-			transform(str.begin(), str.end(), str.begin(), ::toupper);
-
-			cout << str << endl;
-
-			cout << "Do you want to repeat opeartaion (y/n) ?" << endl;
-			cout << "> ";
-			cin >> userRepeatToupper;
-		}
-	}
-	case 4: {
-		unsigned char userRepeatView = 'y';
-		while (userRepeatView == 'y')
-		{
-			string str1("I");
-			string str2("Love");
-			string str3("STL");
-			string str4("String");
-
-			string str5 = str1 + " " + str2 + " " + str3 + " " + str4;
-
-			cout << str5 << endl;
-
-			cout << "Do you want to repeat opeartaion (y/n) ?" << endl;
-			cout << "> ";
-			cin >> userRepeatView;
-		}
-	}
-	case 5: {
-		unsigned char userRepeatView = 'y';
-		while (userRepeatView == 'y')
-		{
-			string str("Good day String! Today is beautiful!");
-
-			cout << str << endl;
-			
-			int sum = 0;
-			for (int i = 0; i < str.length(); i++)
-			{
-				if (str[i]=='a')
-				{
-					sum++;
-				}
-			}
-			cout << "letter a occurenced " << sum << " times" << endl;
-
-			cout << "Do you want to repeat opeartaion (y/n) ?" << endl;
-			cout << "> ";
-			cin >> userRepeatView;
-		}
-	}
-	case 6: {
-		unsigned char userRepeatView = 'y';
-		while (userRepeatView == 'y')
-		{
-			string sampleStr("Good day String! Today is beautiful!");
-			cout << "Sample string is: " << sampleStr << endl;
-			cout << "Locating all instances of character 'a' " << endl;
-
-			auto charPos = sampleStr.find('a', 0);
-
-			while (charPos!=string::npos)
-			{
-				cout << "'" << 'a' << " ' found";
-				cout << " at position: " << charPos << endl;
-
-				size_t charSearchPos = charPos + 1;
-
-				charPos = sampleStr.find('a', charSearchPos);
-			}
-
-			cout << "Do you want to repeat opeartaion (y/n) ?" << endl;
-			cout << "> ";
-			cin >> userRepeatView;
-		}
-	}
-	default:
-		break;
-	}
-
-	
+	cout << "Vector after inserting contents from another vector in the middle: ";
+	DisplayVector(integers);
 
 	return 0;
 }
