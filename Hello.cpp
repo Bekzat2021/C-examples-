5d9287fbbc7c6156a6ce6d04a71d979607cb5c3c
Bekzat2021 <bekzat.work@gmail.com>
2019-11-06 23:04:09 +0500

    Exercises 18.2 ended

--- Hello.cpp
+++ Hello.cpp
@@ -2,73 +2,36 @@
 
 #include "pch.h"
 #include <iostream>
-#include <string>
 #include <list>
+
 using namespace std;
 
 template <typename T>
-void DisplayAsContents(const T &container) {
+void DisplayContents(const T &container) {
 	for (auto element = container.cbegin(); element != container.cend(); element++)
 	{
 		cout << *element << ' ';
-		cout << endl;
 	}
 	cout << endl;
 }
 
-struct ConstactItem
-{
-	string name;
-	string phone;
-	string displayAs;
-
-	ConstactItem(const string &conName, const string &conNum){
-		name = conName;
-		phone = conNum;
-		displayAs = (name + ": " + phone);
-	}
-
-	bool operator == (const ConstactItem &itemToCompare) const {
-		return (itemToCompare.name == this->name);
-	}
-
-	bool operator < (const ConstactItem &itemToCompare) const {
-		return (this->displayAs < itemToCompare.displayAs);
-	}
-
-	operator const char*() const {
-		return displayAs.c_str();
-	}
-};
-
-bool SortOnPhoneNumber(const ConstactItem &item1, const ConstactItem &item2) {
-	return (item1.phone < item2.phone);
-}
-
 int main() {
 
-	list< ConstactItem> contacts;
-	contacts.push_back(ConstactItem("Jack Welsch", "+1 7889879879"));
-	contacts.push_back(ConstactItem("Bill Gates", "+1 97789787998"));
-	contacts.push_back(ConstactItem("Angi Merkel", "+49 234565466"));
-	contacts.push_back(ConstactItem("Vlad Putin", "+7 66454564797"));
-	contacts.push_back(ConstactItem("Dan Creig", "+44 123641976"));
+	list<int> listInts{ 1,2,3,4,5,6 };
 
-	cout << "List in initial order: " << endl;
-	DisplayAsContents(contacts);
+	list<int>::const_iterator iterator1 = listInts.cbegin();
 
-	contacts.sort();
-	cout << "Sorting in alphabetical order via operator<: " << endl;
-	DisplayAsContents(contacts);
+	cout << "iterator1 is: " << *iterator1 << endl;
 
-	contacts.sort(SortOnPhoneNumber);
-	cout << "Sorting in order of phone numbers via predicate: " << endl;
-	DisplayAsContents(contacts);
+	DisplayContents(listInts);
 
-	cout << "Erasing Putin from the list: " << endl;
-	contacts.remove(ConstactItem("Vlad Putin", ""));
-	DisplayAsContents(contacts);
+	listInts.push_front(326);
+
+	cout << "iterator1 is: " << *iterator1 << endl;
+
+	DisplayContents(listInts);
 
 	return 0;
+
 }
 
