affb98ddcc195cf872171e5135f7a92fa9ca848b
Bekzat2021 <bekzat.work@gmail.com>
2019-11-07 23:01:20 +0500

    LISTING 19.5 A Telephone Directory Featuring STL set, find, and erase

--- Hello.cpp
+++ Hello.cpp
@@ -3,43 +3,70 @@
 #include "pch.h"
 #include <set>
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 template <typename T>
-struct SortDescending
-{
-	bool operator()(const T &lhs, const T &rhs) const {
-		return (lhs > rhs);
-	}
-};
-
-template <typename T>
 void DisplayContent(const T &container) {
-	for (auto element = container.cbegin(); element != container.cend(); element++)
+	for (auto iElement = container.cbegin(); iElement != container.cend(); iElement++)
 	{
-		cout << *element << ' ';
+		cout << *iElement << ' '<< endl;
 	}
 	cout << endl;
 }
 
+struct ContactItem
+{
+	string name;
+	string phoneNum;
+	string displayAs;
+
+	ContactItem(const string &nameInit, const string &phone) {
+		name = nameInit;
+		phoneNum = phone;
+		displayAs = (name + ": " + phoneNum);
+	}
+
+	bool operator == (const ContactItem &itemToCompare) const {
+		return (this->name == itemToCompare.name);
+	}
+
+	bool operator < (const ContactItem &itemToCompare) const {
+		return (itemToCompare.name > this->name);
+	}
+
+	operator const char*() const {
+		return displayAs.c_str();
+	}
+};
+
 int main() {
 
-	set<int> setInts{ 202, 151, -999, -1 };
-	setInts.insert(-1);
+	set<ContactItem> setContacts;
+	setContacts.insert(ContactItem("Jack Welsch", "+1 7889 879 879"));
+	setContacts.insert(ContactItem("Bill Gates", "+1 97 7897 8799 8"));
+	setContacts.insert(ContactItem("Angi Merkel", "+49 23456 5466"));
+	setContacts.insert(ContactItem("Vlad Putin", "+7 6645 4564 797"));
+	setContacts.insert(ContactItem("John Travolta", "91 234 4564 789"));
+	setContacts.insert(ContactItem("Ben Affleck", "+1 745 641 314"));
+	DisplayContent(setContacts);
 
-	cout << "Contents of the set: " << endl;
-	DisplayContent(setInts);
+	cout << "Enter a name you wish to delete: ";
+	string inputName;
+	getline(cin, inputName);
+	
+	set<ContactItem>::iterator contactFound = setContacts.find(ContactItem(inputName, ""));
 
-	multiset<int> msetInts;
-	msetInts.insert(setInts.cbegin(), setInts.cend());
-	msetInts.insert(-1);
-
-	cout << "Contents of multiset: " << endl;
-	DisplayContent(msetInts);
-
-	cout << "Number of instances of '-1' in the multiset are: '";
-	cout << msetInts.count(-1) << "'" << endl;
+	if (contactFound!=setContacts.cend())
+	{
+		setContacts.erase(contactFound);
+		cout << "Displaying contents after erasing " << inputName << endl;
+		DisplayContent(setContacts);
+	}
+	else {
+		cout << "Contact not found" << endl;
+	}
 
 	return 0;
 }
